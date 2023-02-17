1070d88cf8d9b2fde852573aae0d04ac77113142
Sean Patrick Wallace <wallace.sean111@gmail.com>
2023-02-17 19:22:17 +0100

    Add code to get and log recent tenders

--- apps/api/tenderServer.js
+++ apps/api/tenderServer.js
@@ -1,4 +1,25 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import pkgSchedule from "node-schedule";
+import getDateTimeString from "./helpers/getDateTimeString.js";
+import getTenders from "./services/getTenders.js";
+
+const runJobs = async () => {
+  console.log(`${getDateTimeString()} - Running jobs...`);
+  console.log(await getTenders());
+}
+
+const { scheduleJob, RecurrenceRule, Range } = pkgSchedule;
+
+const rule = new RecurrenceRule();
+rule.hour = new Range(0, 23, 1);
+
+// const jobsSchedule = scheduleJob(rule, runJobs);
+
+runJobs();
