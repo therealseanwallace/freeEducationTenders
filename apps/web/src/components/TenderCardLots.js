25190765480254f39aab228a700f394475e4f231
Sean Patrick Wallace <wallace.sean111@gmail.com>
2023-03-03 15:14:40 +0100

    Add styles to React app

--- apps/web/src/components/TenderCardLots.js
+++ apps/web/src/components/TenderCardLots.js
@@ -5,12 +5,11 @@ const TenderCardLots = ({ lot }) => {
   let { ID, description, title, duration, value } = lot;
   return (
     <div className="tender-card-lots">
-      <Collapsible trigger={`Lot ${ID}`} className="lot-collapsible">
-        <h2 className="lot-card-id">{}</h2>
+      <Collapsible trigger={`Lot: ${ID}`} className="lot-collapsible">
         <h3 className="lot-card-title">Title: {title}</h3>
-        <h3 className="lot-card-description">Description: {description}</h3>
         <h3 className="lot-card-duration">Duration: {duration}</h3>
         <h3 className="lot-card-value">Value: {value}</h3>
+        <p className="lot-card-description">Description: {description}</p>
       </Collapsible>
     </div>
   );
