9b5a024795e3854499a4f6146484dc77df0b26cc
Sean Patrick Wallace <wallace.sean111@gmail.com>
2023-03-20 02:40:23 +0100

    Modify footer

--- apps/web/src/components/Footer.js
+++ apps/web/src/components/Footer.js
@@ -19,9 +19,9 @@ const Footer = (props) => {
       <div className="footer-top">
         {returnPrivacyPolicyButton()}
         <p className="version-statement">
-          <strong>Version:</strong> 0.3.0 <br />
-          <strong>Please note:</strong> this web application is in a pre-release
-          or “alpha” stage of development and may undergo frequent or extensive
+          <strong>Version:</strong> 0.3.1 <br />
+          <h3>Please note:</h3> this web application is in a pre-release
+          or beta stage of development and may undergo frequent or extensive
           changes or shutdowns or prolonged delays in loading results.
           <br />
           Several features have yet to be added.
@@ -31,14 +31,14 @@ const Footer = (props) => {
       </div>
       <div className="footer-bottom">
         <p className="disclaimer">
+          Contains public sector information licensed under the Open Government
+          Licence v3.0. <br />
           Tenders are sourced from official UK government sources (Find A Tender
           Service and Contracts Finder Service) and used under the{" "}
           <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">
             Open Government Licence
           </a>
           . <br />
-          Contains public sector information licensed under the Open Government
-          Licence v3.0. <br />
           However, this website is NOT an official source and the maintainer of
           this website makes no claims regarding the accuracy or completeness of
           the information contained within. <br /> <br />
